000eb1ba0b493c7b6436de94759fc2d04d600a6a
djanos321 <107697234+djanos321@users.noreply.github.com>
2022-07-06 22:33:28 +0200

    Add files via upload

---
+++ app/config/auth.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    secret: "janos-titkos-kulcsa"
+  };
+  
---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "localhost",
+    FELHASZNALO: "root",
+    JELSZO: "root",
+    AB: "abrakoczi",
+    nyelv: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
